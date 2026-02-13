1597eea85632ab9ba0f21c5ab8f342cc46f4e745
Munir Horvat <h8.munir@gmail.com>
2026-02-13 14:52:34 +0100

    Add cinema management console application
    This Java program simulates a simple cinema management system via the console, allowing users to view movies, purchase tickets, and validate inputs.

---
+++ cinema-management/KinoVerwaltung1.java
@@ -0,0 +1,23 @@
+# Kino Verwaltung (Konsolenanwendung)
+
+Dieses kleine Java-Programm simuliert eine einfache Kinoverwaltung über die Konsole.
+
+## Funktionen
+- Anzeige der verfügbaren Filme mit Uhrzeit, Saal und Restplätzen
+- Auswahl eines Films durch Eingabe der Filmnummer
+- Ticketkauf (15€ pro Ticket)
+- Überprüfung des verfügbaren Geldes
+- Überprüfung der verfügbaren Sitzplätze
+- Reduzierung der Restplätze nach dem Kauf
+- Einfache Eingabevalidierung
+
+## Gelernt / Angewendet
+- Arrays
+- Schleifen (for, while, do-while)
+- Bedingungen (if / else)
+- Methodenstruktur
+- ArrayList
+- Grundlegende Benutzerinteraktion mit Scanner
+
+## Ziel
+Übung grundlegender Java-Konzepte sowie strukturierte Programmierung in einer realitätsnahen Aufgabe.
